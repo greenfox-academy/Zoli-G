e6e7ec3214f11d6efda849ca3d381b4ee486fc49
Gálla Zoltán <galla.zoltan.tamas@gmail.com>
2017-10-10 12:46:41 +0200

    Day 7 - User input with getch

---
+++ week-02/day-2/input_2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    char buffer[255];   // Buffer variable for user input
+
+    //TODO:
+    // Get the user's name with gets
+    printf("Please enter your name: ");
+    gets(buffer);
+
+    //TODO:
+    // Print it out with puts
+    printf("\n\nHello ");
+    puts(buffer);
+
+    //TODO:
+    // Get the user's pet name with getch
+    printf("\n\nWhat's your pets name?: ");
+    int ch, i = 0;
+    char petname[255];
+
+    do {
+        ch = getch();
+        petname[i] = ch;
+        printf("%c", ch);
+        i++;
+    } while (ch != 13);
+
+    petname[i - 1] = 0;
+
+    printf("\nYour pets name is %s.\n", petname);
+
+    //TODO:
+    // Print it out
+
+    return 0;
+}

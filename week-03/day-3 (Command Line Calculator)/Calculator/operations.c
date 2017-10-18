89b780a00eba5d7cb1479de9124f846da432588f
Gálla Zoltán <galla.zoltan.tamas@gmail.com>
2017-10-18 10:26:14 +0200

    Day 13 - Command line calculator project initialized with files.

---
+++ week-03/day-3 (Command Line Calculator)/Calculator/operations.c
@@ -0,0 +1,25 @@
+void print_menu() {
+	printf("           CLI Calculator           \n");
+	printf("====================================\n");
+	printf("usage: [number] [operation] [number]\n");
+	printf("Commands:\n");
+	printf("\n");
+	printf("+	summation\n");
+	printf("-	subtraction\n");
+	printf("*	multiplication\n");
+	printf("/	division\n");
+	printf("%	division with remainder\n");
+	printf("^	squaring\n");
+	printf("<	square root\n");
+	printf("log	logarithm\n");
+	printf("binto	binary to hex or dec\n");
+	printf("hexto	hexadecimal to bin or dec\n");
+	printf("decto	decimal to bin or hex\n");
+	printf("====================================\n");
+	printf("exit	exiting from the program\n");
+	printf("clear	clear the screen\n");
+	printf("help	print usage\n");
+	printf("====================================\n");
+	printf("Hit enter to start!\n");
+	printf("====================================\n");
+}

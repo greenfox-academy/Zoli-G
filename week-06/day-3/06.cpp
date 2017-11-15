326c93b15c4fec2d9bf72b0bb57baad894c3f75b
Gálla Zoltán <galla.zoltan.tamas@gmail.com>
2017-11-15 17:05:58 +0100

    C++ inheritance: 6 (Linked list) finished

---
+++ week-06/day-3/06.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Create a linked list using classes.
+// If you are not similar whit linked lists look it up on the internet.
+// Feel free to walk through some tutorials, get familiar with your implementation.
+
+struct Node {
+    int x;
+    Node *next;
+};
+
+class LinkedList{
+public:
+    LinkedList() {
+      tail = NULL;              //set tail as NULL
+      head->next = tail;        //set head to point to tail (close link)
+
+    }
+
+    void addValue(int val) {
+        Node *n = new Node();   //create a new Node
+        head->x = val;          //set head value to the added parameter
+        n->next = head;         //set the new node to point back to head
+        head = n;               //set head to the latest node
+    }
+
+    void display() {
+      Node *temp = new Node();
+      temp = head->next;
+      while (temp->next != NULL) {
+        cout << temp->x << " ";
+        temp = temp->next;
+      }
+      cout << temp->x;
+    }
+
+private:
+    Node *head, *tail;
+};
+
+int main()
+{
+    LinkedList l;
+    l.addValue(5);
+    l.addValue(23);
+    l.addValue(9);
+    l.addValue(111);
+    l.display();
+
+    return 0;
+}

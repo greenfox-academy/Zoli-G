6ed3f012876ef03810940f82a8cef9776b444f89
Gálla Zoltán <galla.zoltan.tamas@gmail.com>
2017-11-20 14:37:11 +0100

    Week 7 - C++ Exception task 4 (throw int in function) finished

---
+++ week-07/day-1/Exceptions-04.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Create a function which throws an integer
+// In the main(), put that function in a try block
+// Also in the main() catch, what your function throws
+float division(int a, int b) throw(int) {
+	if (b == 0) {
+		throw 0;
+	}
+	return a / b;
+}
+
+int main() {
+
+	try {
+		cout << "Result: " << division(89, 0) << endl;
+	} catch(int e) {
+		switch (e) {
+			case 0 : cout << "Error (" << e << ") : zero divisor." << endl;
+					 break;
+		}
+	}
+	return 0;
+}

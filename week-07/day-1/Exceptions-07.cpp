ff9c44bcb102974a17bc95a98fbf6bc36afa733b
Gálla Zoltán <galla.zoltan.tamas@gmail.com>
2017-11-20 14:38:56 +0100

    Week 7 - C++ Exception task 7 (array index error) finished

---
+++ week-07/day-1/Exceptions-07.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+//Try to run the following code!
+
+int main () {
+
+    int int_array[5] = {1, 2, 3, 4, 5};
+    
+    try {
+    	int n = 200;
+
+    	if (n > 5) {
+    		throw 'i';
+    	}
+
+    	cout << int_array[n];
+    } catch(char e) {
+    	cout << "Array index error (" << e << "): out of range." << endl;
+    }
+    return 0;
+}
+
+//The program should compile but crash!
+//Create a solution for this problem using try-catch block!

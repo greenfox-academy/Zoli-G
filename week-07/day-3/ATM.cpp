00b809b2e9fa5a330bfa115d2213c80f2b328098
Gálla Zoltán <galla.zoltan.tamas@gmail.com>
2017-11-22 15:27:15 +0100

    Week 7 - C++ ATM task continued...

--- week-07/day-3/ATM.cpp
+++ week-07/day-3/ATM.cpp
@@ -2,18 +2,6 @@
 #include <vector>
 using namespace std;
 
-class ATM {
-private:
-    string admin_password = "admin123";
-public:
-    string getATMAdminPass() {
-        return admin_password;
-    }
-    //bool validATMPassword(string inputPass) {
-    //    return (inputPass == admin_password ? true : false);
-    //}
-};
-
 class user_data {
 public:
     user_data(string name, unsigned int pincode, float money) : name(name), pincode(pincode), money(money) {}
@@ -23,25 +11,54 @@ public:
     string getName() {
         return name;
     }
+    unsigned int getPincode() {
+        return pincode;
+    }
 private:
     string name;
     unsigned int pincode;
     float money;
 };
-
+//------------------------------
 class Users {
 private:
     vector<user_data> UserList;
+    //ATM password
+    string admin_password = "admin123";
 public:
     void addUser(user_data ud) {
         UserList.push_back(ud);
     }
+    //string getATMAdminPass() {
+    //    return admin_password;
+    //}
     unsigned int getUserPin(unsigned int index) {
-        return UserList.at(index).pincode;
+        return UserList.at(index).getPincode();
     }
     unsigned int getUserCount() {
         return UserList.size();
     }
+    //Check if  it is an ATM admin password
+    int ValidatePassInput(string input_pass) {
+        if (input_pass == admin_password) {
+            cout << "Admin logged in." << endl;
+            return 9999;    //Admin logged in
+        }
+        cout << "Admin login failed." << endl;
+        return -1;          //False password
+    }
+    //Check if it is a user password
+    int ValidateInput(unsigned int input_pass) {
+        for (int i = 0; i < UserList.size(); ++i) {
+            if (input_pass == UserList.at(i).getPincode()) {
+                cout << "User " << UserList.at(i).getName() << " logged in." << endl;
+                return i;    //User logged in, return user index in vector
+            }
+        }
+        cout << "User login failed." << endl;
+        return -1;          //False password
+    }
+    //Print richest customer
     void printRichestCostumer() {
         float max_money = 0;
         unsigned int max_index = 0;
@@ -55,13 +72,10 @@ public:
     }
 };
 
-class Customer : public Users {
-
-};
-
-class Administrator : public Users {
-
-};
+//Function prototypes
+void MainScreen();
+int ValidateInput(string);
+int ValidateInput(unsigned int);
 
 int main() {
 
@@ -74,40 +88,18 @@ int main() {
     BankUsers.addUser(u2);
     BankUsers.addUser(u3);
 
-    ATM BankATM;
-
     BankUsers.printRichestCostumer();
+
+    MainScreen();
+    unsigned int input_pin = 0;
+    cin >> input_pin;
+    BankUsers.ValidateInput(input_pin);
 
     return 0;
 }
 
 void MainScreen() {
-    unsigned int input_pin = 0;
-
     cout << "Welcome to the Z-Bank" << endl;
     cout << "---------------------" << endl;
     cout << "Please enter your PIN code: " << endl;
-    cin >> input_pin;
-
-    if () {
-        cout << ""
-    }
-}
-
-//Check if  it is an ATM admin password
-int ValidateInput(string input_pass) {
-    if (input_pass == BankATM.getATMAdminPass()) {
-        return 9999;    //Admin logged in
-    }
-    return -1;          //False password
-}
-
-//Check if it is a user password
-int ValidateInput(unsigned int input_pass) {
-    for (int i = 0; i < BankUsers.getUserCount(); ++i) {
-        if (input_pass == BankUsers.getUserPin(i)) {
-            return i;    //User logged in, return user index in vector
-        }
-    }
-    return -1;          //False password
 }

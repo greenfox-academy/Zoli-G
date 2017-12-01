b81d6dd16889b57dcac4c33bf7102cbe44dae009
Gálla Zoltán <galla.zoltan.tamas@gmail.com>
2017-12-01 12:31:02 +0100

    STM32 Discovery Board Project LED-blinker projects updated with 6 bits

---
+++ week-08/day-3/led_blinker-6bit_digital_counter_while_button_press/Src/main.cpp
@@ -0,0 +1,345 @@
+ /**
+  ******************************************************************************
+  * @file    Templates/Src/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.3
+  * @date    22-April-2016 
+  * @brief   STM32F7xx HAL API Template project 
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <unistd.h>
+
+/** @addtogroup STM32F7xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup Templates
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+static void SystemClock_Config(void);
+static void Error_Handler(void);
+static void MPU_Config(void);
+static void CPU_CACHE_Enable(void);
+
+void LedInit() {
+  __HAL_RCC_GPIOA_CLK_ENABLE();    // Enable the GPIOA port's clock first
+
+  GPIO_InitTypeDef LED0;            // create a config structure
+  LED0.Pin = GPIO_PIN_0;            // this is about PIN 0
+  LED0.Mode = GPIO_MODE_OUTPUT_PP;  // Configure as output with push-up-down enabled
+  LED0.Pull = GPIO_PULLDOWN;        // the push-up-down should work as pulldown
+  LED0.Speed = GPIO_SPEED_HIGH;     // we need a high-speed output
+
+  HAL_GPIO_Init(GPIOA, &LED0);      // initialize the pin on GPIOA port with HAL
+
+  __HAL_RCC_GPIOF_CLK_ENABLE();    // Enable the GPIOA port's clock first
+
+    GPIO_InitTypeDef LED1;            // create a config structure
+    LED1.Pin = GPIO_PIN_10 | GPIO_PIN_9 | GPIO_PIN_8 | GPIO_PIN_7 | GPIO_PIN_6;            // this is about PIN 0
+    LED1.Mode = GPIO_MODE_OUTPUT_PP;  // Configure as output with push-up-down enabled
+    LED1.Pull = GPIO_PULLDOWN;        // the push-up-down should work as pulldown
+    LED1.Speed = GPIO_SPEED_HIGH;     // we need a high-speed output
+
+    HAL_GPIO_Init(GPIOF, &LED1);
+/*
+    GPIO_InitTypeDef LED2;            // create a config structure
+    LED2.Pin = GPIO_PIN_9;            // this is about PIN 0
+    LED2.Mode = GPIO_MODE_OUTPUT_PP;  // Configure as output with push-up-down enabled
+    LED2.Pull = GPIO_PULLDOWN;        // the push-up-down should work as pulldown
+    LED2.Speed = GPIO_SPEED_HIGH;     // we need a high-speed output
+
+    HAL_GPIO_Init(GPIOF, &LED2);
+
+    GPIO_InitTypeDef LED3;            // create a config structure
+    LED3.Pin = GPIO_PIN_8;            // this is about PIN 0
+    LED3.Mode = GPIO_MODE_OUTPUT_PP;  // Configure as output with push-up-down enabled
+    LED3.Pull = GPIO_PULLDOWN;        // the push-up-down should work as pulldown
+    LED3.Speed = GPIO_SPEED_HIGH;     // we need a high-speed output
+
+    HAL_GPIO_Init(GPIOF, &LED3);
+
+    GPIO_InitTypeDef LED4;            // create a config structure
+    LED4.Pin = GPIO_PIN_7;            // this is about PIN 0
+    LED4.Mode = GPIO_MODE_OUTPUT_PP;  // Configure as output with push-up-down enabled
+    LED4.Pull = GPIO_PULLDOWN;        // the push-up-down should work as pulldown
+    LED4.Speed = GPIO_SPEED_HIGH;     // we need a high-speed output
+
+    HAL_GPIO_Init(GPIOF, &LED4);
+
+    GPIO_InitTypeDef LED5;            // create a config structure
+    LED5.Pin = GPIO_PIN_6;            // this is about PIN 0
+    LED5.Mode = GPIO_MODE_OUTPUT_PP;  // Configure as output with push-up-down enabled
+    LED5.Pull = GPIO_PULLDOWN;        // the push-up-down should work as pulldown
+    LED5.Speed = GPIO_SPEED_HIGH;     // we need a high-speed output
+
+    HAL_GPIO_Init(GPIOF, &LED5);*/
+
+    //Button on CN7-D9 (A port)
+    GPIO_InitTypeDef Button;
+    Button.Pin = GPIO_PIN_15;
+    Button.Mode = GPIO_MODE_INPUT;
+    Button.Pull = GPIO_PULLUP;
+    Button.Speed = GPIO_SPEED_HIGH;
+
+    HAL_GPIO_Init(GPIOA, &Button);
+}
+
+void HalfByteLED(unsigned int x) {
+	//unsigned int maskA0, maskF10, maskF9, maskF8, maskF7, maskF6,
+	unsigned int maskA0, maskF10_F6;
+
+	maskA0 = 0b000001;
+	GPIOA->ODR &= 0b11111111111111111111111111111110;
+	GPIOA->ODR |= (x & maskA0);
+
+	maskF10_F6 = 0b111110;
+	GPIOF->ODR &= 0b11111111111111111111100000111111;
+	GPIOF->ODR |= ((x & maskF10_F6) >> 1) << 6;
+
+	/*
+	maskF10 = 0b000010;
+	GPIOF->ODR &= 0b11111111111111111111101111111111;
+	GPIOF->ODR |= ((x & maskF10) >> 1) << 10;
+
+	maskF9 = 0b000100;
+	GPIOF->ODR &= 0b11111111111111111111110111111111;
+	GPIOF->ODR |= ((x & maskF9) >> 2) << 9;
+
+	maskF8 = 0b001000;
+	GPIOF->ODR &= 0b11111111111111111111111011111111;
+	GPIOF->ODR |= ((x & maskF8) >> 3) << 8;
+
+	maskF7 = 0b010000;
+	GPIOF->ODR &= 0b11111111111111111111111101111111;
+	GPIOF->ODR |= ((x & maskF7) >> 4) << 7;
+
+	maskF6 = 0b100000;
+	GPIOF->ODR &= 0b11111111111111111111111110111111;
+	GPIOF->ODR |= ((x & maskF6) >> 5) << 6;
+	*/
+	HAL_Delay(200);
+
+}
+
+int main(void)
+{
+
+  /* This project template calls firstly two functions in order to configure MPU feature 
+     and to enable the CPU Cache, respectively MPU_Config() and CPU_CACHE_Enable().
+     These functions are provided as template implementation that User may integrate 
+     in his application, to enhance the performance in case of use of AXI interface 
+     with several masters. */ 
+  
+  /* Configure the MPU attributes as Write Through */
+  MPU_Config();
+
+  /* Enable the CPU Cache */
+  CPU_CACHE_Enable();
+
+  /* STM32F7xx HAL library initialization:
+       - Configure the Flash ART accelerator on ITCM interface
+       - Configure the Systick to generate an interrupt each 1 msec
+       - Set NVIC Group Priority to 4
+       - Low Level Initialization
+     */
+  HAL_Init();
+
+  /* Configure the System clock to have a frequency of 216 MHz */
+  SystemClock_Config();
+
+  /* Add your application code here     */
+  LedInit();
+  unsigned int i = 0;
+  /* Infinite loop */
+  while (1) {
+	  if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_15) == GPIO_PIN_RESET) {
+		  HalfByteLED(i);
+		  ++i;
+	  }
+  }
+}
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 216000000
+  *            HCLK(Hz)                       = 216000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 25000000
+  *            PLL_M                          = 25
+  *            PLL_N                          = 432
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 9
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 7
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 432;  
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 9;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* activate the OverDrive to reach the 216 Mhz Frequency */
+  if(HAL_PWREx_EnableOverDrive() != HAL_OK)
+  {
+    Error_Handler();
+  }
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+  /* User may add here some code to deal with this error */
+  while(1)
+  {
+  }
+}
+
+/**
+  * @brief  Configure the MPU attributes as Write Through for SRAM1/2.
+  * @note   The Base Address is 0x20010000 since this memory interface is the AXI.
+  *         The Region Size is 256KB, it is related to SRAM1 and SRAM2  memory size.
+  * @param  None
+  * @retval None
+  */
+static void MPU_Config(void)
+{
+  MPU_Region_InitTypeDef MPU_InitStruct;
+  
+  /* Disable the MPU */
+  HAL_MPU_Disable();
+
+  /* Configure the MPU attributes as WT for SRAM */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.BaseAddress = 0x20010000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_256KB;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+  MPU_InitStruct.SubRegionDisable = 0x00;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+  /* Enable the MPU */
+  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+}
+
+/**
+  * @brief  CPU L1-Cache enable.
+  * @param  None
+  * @retval None
+  */
+static void CPU_CACHE_Enable(void)
+{
+  /* Enable I-Cache */
+  SCB_EnableICache();
+
+  /* Enable D-Cache */
+  SCB_EnableDCache();
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/

a3bfab03f3d805874758facca5d70e39873a5150
Gálla Zoltán <galla.zoltan.tamas@gmail.com>
2017-12-06 17:32:31 +0100

    STM32: Dynamo task (Timer, PWM, IRQ, LED, button) finished

--- week-09/day-3/Dynamo-LED/src/main.c
+++ week-09/day-3/Dynamo-LED/src/main.c
@@ -1,18 +1,29 @@
 #include "main.h"
 
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
 //Function prototypes
 void LEDInit();
-void TimerInit();
+void TimerPWMInit();
 void TimerITInit();
+void TIM2_IRQHandler();
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
 void Button_IT_Init();
 void EXTI15_10_IRQHandler();
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
+void UARTInit();
 
 //Global objects
 GPIO_InitTypeDef PWMPinConfig;
 GPIO_InitTypeDef Button;
 TIM_HandleTypeDef Timer;
+TIM_HandleTypeDef TimerIT;
 TIM_OC_InitTypeDef TimerOC;
+UART_HandleTypeDef uart_handle;
 
 int main(void) {
 	//Hardware config
@@ -27,7 +38,9 @@ int main(void) {
 
 	LEDInit();
 	Button_IT_Init();
-	TimerInit();
+	TimerPWMInit();
+	TimerITInit();
+	UARTInit();
 
 	while (1) {
 		
@@ -47,12 +60,12 @@ void LEDInit() {
 	HAL_GPIO_Init(GPIOA, &PWMPinConfig);
 }
 
-void TimerInit() {
+void TimerPWMInit() {
 	__HAL_RCC_TIM1_CLK_ENABLE();
 
 	Timer.Instance = TIM1;
 	Timer.Init.Period = 1000;
-	Timer.Init.Prescaler = 0;
+	Timer.Init.Prescaler = 1;
 	Timer.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 	Timer.Init.CounterMode = TIM_COUNTERMODE_UP;
 
@@ -62,6 +75,32 @@ void TimerInit() {
 	HAL_TIM_PWM_Init(&Timer);
 	HAL_TIM_PWM_ConfigChannel(&Timer, &TimerOC, TIM_CHANNEL_1);
 	HAL_TIM_PWM_Start(&Timer, TIM_CHANNEL_1);
+}
+
+void TimerITInit() {
+	__HAL_RCC_TIM2_CLK_ENABLE();
+
+	TimerIT.Instance = TIM2;
+	TimerIT.Init.Period = 40;
+	TimerIT.Init.Prescaler = 54000;
+	TimerIT.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	TimerIT.Init.CounterMode = TIM_COUNTERMODE_UP;
+
+	HAL_TIM_Base_Init(&TimerIT);
+	HAL_TIM_Base_Start_IT(&TimerIT);
+
+	HAL_NVIC_SetPriority(TIM2_IRQn, 0x0F, 0x00);
+	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+void TIM2_IRQHandler() {
+	HAL_TIM_IRQHandler(&TimerIT);
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	TIM1->CCR1 >= 1 ? (TIM1->CCR1 -= 1) : (TIM1->CCR1 = 0);
+    if (TIM1->CCR1 % 10 == 0 && TIM1->CCR1 != 0)
+    	printf("Timer interrupt -> LED fade (%lu).\n", TIM1->CCR1);
 }
 
 void Button_IT_Init() {
@@ -74,7 +113,7 @@ void Button_IT_Init() {
 
 	HAL_GPIO_Init(GPIOA, &Button);
 
-	HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0x0F, 0x00);
+	HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0x1F, 0x00);
 	HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 }
 
@@ -83,5 +122,25 @@ void EXTI15_10_IRQHandler() {
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-	TIM1->CCR1 += 50;
+	TIM1->CCR1 <= 990 ? (TIM1->CCR1 += 10) : (TIM1->CCR1 = 1000);
+	printf("Button interrupt -> LED shine! (%lu)\n", TIM1->CCR1);
+}
+
+void UARTInit() {
+	uart_handle.Init.BaudRate = 115200;
+	uart_handle.Init.WordLength = UART_WORDLENGTH_8B;
+	uart_handle.Init.StopBits = UART_STOPBITS_1;
+	uart_handle.Init.Parity = UART_PARITY_NONE;
+	uart_handle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	uart_handle.Init.Mode = UART_MODE_TX_RX;
+
+	BSP_COM_Init(COM1, &uart_handle);
+}
+
+PUTCHAR_PROTOTYPE {
+	/* Place your implementation of fputc here */
+	/* e.g. write a character to the EVAL_COM1 and Loop until the end of transmission */
+	HAL_UART_Transmit(&uart_handle, (uint8_t *) &ch, 1, 0xFFFF);
+
+	return ch;
 }

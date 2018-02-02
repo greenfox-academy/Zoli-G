4d0c2dab88ec2d0438aaa0e6893a4cd3f9b6dd7d
GZoli <galla.zoltan.tamas@gmail.com>
2018-02-02 09:47:18 +0100

    Final exam task 8 - File initialized

---
+++ Final_Exam/Exam_task_8.c
@@ -0,0 +1,17 @@
+/* Static- Final Exam
+   2018.02.02.
+   8. Create a function that takes two arrays and mixes them into a third one
+   like this: [1, 2, 3] and [6, 7, 8] becomes [1, 6, 2, 7, 3, 8]
+   (order does not matter)
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+
+void main() {
+    uint8_t num_array_a[4] = {0, 45, 6, 2};
+    uint8_t num_array_b[3] = {1, 23, 11};
+
+
+    printf("%d", sizeof(num_array_a));
+}

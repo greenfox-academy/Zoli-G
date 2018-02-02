7f875a8af867f76830469b1a9cbaa80b71b34cd0
GZoli <galla.zoltan.tamas@gmail.com>
2018-02-02 10:13:36 +0100

    Final exam task 8 - Function

--- Final_Exam/Exam_task_8.c
+++ Final_Exam/Exam_task_8.c
@@ -8,10 +8,36 @@
 #include <stdio.h>
 #include <stdint.h>
 
+uint8_t* make_union_array(uint8_t a[], uint8_t sizea, uint8_t b[], uint8_t sizeb);
+
 void main() {
-    uint8_t num_array_a[4] = {0, 45, 6, 2};
-    uint8_t num_array_b[3] = {1, 23, 11};
+    //Variables
+    uint8_t num_array_a[5] = {0, 45, 6, 2, 5};
+    uint8_t num_array_b[4] = {1, 23, 11, 7};
 
+    uint8_t* p;
 
-    printf("%d", sizeof(num_array_a));
+    p = make_union_array(num_array_a, 5, num_array_b, 4);
+
+    printf("%d", *p);
+}
+
+uint8_t* make_union_array(uint8_t a[], uint8_t sizea, uint8_t b[], uint8_t sizeb) {
+
+    uint8_t sizes = sizea + sizeb;
+
+    printf("Total no. of elements in the two array are: %d\n", sizes);
+
+    uint8_t union_array[sizes];
+    for (uint8_t i = 0; i < sizea; ++i) {
+        union_array[i] = a[i];
+    }
+    for (uint8_t i = 0; i < sizeb; ++i) {
+        union_array[i + sizea] = b[i];
+    }
+
+    for (uint8_t i = 0; i < sizes; ++i) {
+        printf("%d ", union_array[i]);
+    }
+    return union_array;
 }
